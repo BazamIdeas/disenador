f56844353339e9eda6bbce32762dc91af70a92e6
Xavier Arias <xarias13@gmail.com>
2017-01-27 14:34:55 -0400

    Primera ruta angular

--- public/creador-de-logos/app/app.js
+++ public/creador-de-logos/app/app.js
@@ -1,3 +1,14 @@
 angular.module("disenador-de-logos", ["angular-messages", "angular-ui-router"])
 
-.config();
+.config(function($stateProvider){
+    
+    
+    var inicio = {
+        name: 'inicio',
+        url: '/',
+        template: '<h3>hello world!</h3>'
+    }
+    
+    
+     $stateProvider.state(inicio);
+});
