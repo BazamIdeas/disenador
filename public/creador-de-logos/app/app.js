3136c78ec973d306fff3f21a35a32b71a8aa38f2
Luis Torres <luisdaniel.programador@gmail.com>
2017-02-02 22:15:07 -0400

    Revert "Primera ruta angular"
    This reverts commit f56844353339e9eda6bbce32762dc91af70a92e6.

--- public/creador-de-logos/app/app.js
+++ public/creador-de-logos/app/app.js
@@ -1,14 +1,3 @@
 angular.module("disenador-de-logos", ["angular-messages", "angular-ui-router"])
 
-.config(function($stateProvider){
-    
-    
-    var inicio = {
-        name: 'inicio',
-        url: '/',
-        template: '<h3>hello world!</h3>'
-    }
-    
-    
-     $stateProvider.state(inicio);
-});
+.config();
