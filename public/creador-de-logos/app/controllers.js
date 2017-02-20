e5704d39b791041ae36a41a1648a06a70a233b4f
Luis Torres <luisdaniel.programador@gmail.com>
2017-02-20 10:44:34 -0400

    Resumida la funcion de dialogos
    Elimanada cacteristicas.tpl, categorias.tpl y etiquetas.tpl
    Creada dialogoOpciones.tpl

--- public/creador-de-logos/app/controllers.js
+++ public/creador-de-logos/app/controllers.js
@@ -40,7 +40,7 @@ angular.module("disenador-de-logos")
 
 /* Opciones */
 
-.controller('opcionesController', ['$scope', '$mdDialog', "$stateParams", function ($scope, $mdDialog, $stateParams) {
+.controller('opcionesController', ['$scope', '$mdDialog', "$stateParams", function ($scope, $mdDialog, $stateParams, $timeout) {
 
 
     this.datosEstadoAnterior = $stateParams.datos;
@@ -190,86 +190,48 @@ angular.module("disenador-de-logos")
 
     /* Modal */
 
-    this.estado = false;
-    this.modoSeleccionado = 'md-scale';
+    this.hidden = false;
+    this.isOpen = false;
+    this.hover = false;
+    
+    this.elementosDialog = [
+        {
+            name: "Caracteristicas",
+            icon: "face",
+            direction: "bottom"
+        },
+        {
+            name: "Etiquetas",
+            icon: "label",
+            direction: "top"
+        },
+        {
+            name: "Categorias",
+            icon: "lightbulb_outline",
+            direction: "bottom"
+        }
+      ];
 
-    this.status = '  ';
-    this.customFullscreen = false;
-    this.showAlert = function (ev) {
-        // Appending dialog to document.body to cover sidenav in docs app
-        // Modal dialogs should fully cover application
-        // to prevent interaction outside of dialog
+    this.abrirDialogo = function ($event, elementoDialog) {
 
-        $mdDialog.show(
-            $mdDialog.alert()
-            .parent(angular.element(document.querySelector('#popupContainer')))
-            .clickOutsideToClose(true)
-            .title('This is an alert title')
-            .textContent('You can specify some description text in here.')
-            .ariaLabel('Alert Dialog Demo')
-            .ok('Got it!')
-            .targetEvent(ev)
-        );
-    };
-    this.Categorias = function (ev) {
         $mdDialog.show({
-                controller: DialogController,
-                templateUrl: 'app/views/dialogos/categorias.tpl',
-                parent: angular.element(document.body),
-                targetEvent: ev,
-                clickOutsideToClose: true,
-                fullscreen: this.customFullscreen // Only for -xs, -sm breakpoints.
-            })
-            .then(function (answer) {
-                this.status = 'You said the information was "' + answer + '".';
-            }, function () {
-                this.status = 'You cancelled the dialog.';
-            });
-    };
-    this.Etiquetas = function (ev) {
-        $mdDialog.show({
-                controller: DialogController,
-                templateUrl: 'app/views/dialogos/etiquetas.tpl',
-                parent: angular.element(document.body),
-                targetEvent: ev,
-                clickOutsideToClose: true,
-                fullscreen: this.customFullscreen // Only for -xs, -sm breakpoints.
-            })
-            .then(function (answer) {
-                this.status = 'You said the information was "' + answer + '".';
-            }, function () {
-                this.status = 'You cancelled the dialog.';
-            });
-    };
-    this.Caracteristicas = function (ev) {
-        $mdDialog.show({
-                controller: DialogController,
-                templateUrl: 'app/views/dialogos/caracteristicas.tpl',
-                parent: angular.element(document.body),
-                targetEvent: ev,
-                clickOutsideToClose: true,
-                fullscreen: this.customFullscreen // Only for -xs, -sm breakpoints.
-            })
-            .then(function (answer) {
-                this.status = 'You said the information was "' + answer + '".';
-            }, function () {
-                this.status = 'You cancelled the dialog.';
-            });
-    };
+            clickOutsideToClose: true,
+            controller: function ($mdDialog) {
 
-    function DialogController($scope, $mdDialog) {
-        $scope.hide = function () {
-            $mdDialog.hide();
-        };
-        $scope.cancel = function () {
-            $mdDialog.cancel();
-        };
-        $scope.answer = function (answer) {
-            $mdDialog.hide(answer);
-        };
+                this.elementoDialog = elementoDialog;
 
+                this.cerrarDialogo = function () {
+                    $mdDialog.cancel();
+                };
+                this.enviarDialogo = function () {
+                    $mdDialog.hide();
+                };
+            },
+            controllerAs: 'dialog',
+            templateUrl: 'app/views/dialogos/dialogoOpciones.tpl',
+            targetEvent: $event
+        });
     };
-
 }])
 
 /* Proceso */
--- public/creador-de-logos/app/views/opciones.tpl
+++ public/creador-de-logos/app/views/opciones.tpl
@@ -1,29 +1,37 @@
 <style ng-repeat="fuente in opciones.respuesta.fuentes">
     @font-face {
-        font-family: {{fuente.nombre}};
+        font-family: {
+            {
+                fuente.nombre
+            }
+        }
+        ;
         src: url('{{fuente.url}}');
     }
+
 </style>
 <div flex layout="column" ng-cloak>
     <div layout="row" layout-align="space-between center" id="popupContainer">
         <h3 flex="40" class="titulo_opciones md-whiteframe-2dp" md-truncate>ELIJA LOS DISEÑOS Y LAS FUENTES QUE DESEA USAR</h3>
         <div flex layout layout-align="end">
-            <md-fab-speed-dial md-open="false" md-direction="left" ng-class="opciones.modoSeleccionado" ng-click="opciones.estado=true">
+            <md-fab-speed-dial ng-hide="opciones.hidden" md-direction="left" md-open="opciones.isOpen" class="md-scale md-fab-top-right" ng-class="{ 'md-hover-full': opciones.hover }" ng-mouseenter="opciones.isOpen=true" ng-mouseleave="opciones.isOpen=false">
                 <md-fab-trigger>
                     <md-button aria-label="menu" class="md-fab md-warn">
+                        <md-tooltip md-direction="top" md-visible="tooltipVisible">Menu</md-tooltip>
                         <md-icon>build</md-icon>
                     </md-button>
                 </md-fab-trigger>
+
                 <md-fab-actions>
-                    <md-button aria-label="Face" class="md-fab md-raised md-mini" ng-click="opciones.Categorias($event)">
-                        <md-icon>face</md-icon>
-                    </md-button>
-                    <md-button aria-label="Label" class="md-fab md-raised md-mini" ng-click="opciones.Etiquetas($event)">
-                        <md-icon>label</md-icon>
-                    </md-button>
-                    <md-button aria-label="Algo" class="md-fab md-raised md-mini" ng-click="opciones.Caracteristicas($event)">
-                        <md-icon>lightbulb_outline</md-icon>
-                    </md-button>
+                    <div ng-repeat="elementoDialog in opciones.elementosDialog">
+                        <md-button aria-label="{{elementoDialog.name}}" class="md-fab md-raised md-mini" ng-click="opciones.abrirDialogo($event, elementoDialog)">
+                            <md-tooltip md-direction="{{elementoDialog.direction}}" md-visible="tooltipVisible" md-autohide="false">
+                                {{elementoDialog.name}}
+                            </md-tooltip>
+
+                            <md-icon aria-label="{{elementoDialog.name}}">{{elementoDialog.icon}}</md-icon>
+                        </md-button>
+                    </div>
                 </md-fab-actions>
             </md-fab-speed-dial>
         </div>
@@ -39,7 +47,7 @@
         <div layout="row" flex="45" class="md-whiteframe-2dp scroll ">
             <div flex="100" layout="row" layout-align="baseline space-around" layout-wrap layout-padding class="contenedor_fuentes text-center">
                 <div flex="33" layout-padding ng-click="opciones.agregarElemento($index, fuente, 'fuentes')" ng-repeat="fuente in opciones.respuesta.fuentes">
-                    <p class="fuente"ng-style="{'font-family' : fuente.nombre}" ng-class="opciones.datos.fuentes[$index].estado">{{opciones.datosEstadoAnterior.nombre}}</p>
+                    <p class="fuente" ng-style="{'font-family' : fuente.nombre}" ng-class="opciones.datos.fuentes[$index].estado">{{opciones.datosEstadoAnterior.nombre}}</p>
                 </div>
             </div>
         </div>
