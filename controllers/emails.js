8c06fa2405f1723dc45f21a1216c84ec1579a41b
michel <michelnovellino.programador@gmail.com>
2017-03-09 15:55:31 -0400

    Modulo etiquetas
    Commit del modulo de etiquetas

--- controllers/emails.js
+++ controllers/emails.js
@@ -16,8 +16,10 @@ exports.enviar_email= function(req,res){
 // Configuracion de los datos del correo
  var mailOptions = {
     from: '"Liderlogo 👻" <michelnovellino.programador@gmail.com>', // remitente
-    to: 'michelnovellino.programador@gmail.com', // receptor o receptores
+    to: 'michelnovellino.programador@gmail.com,kalinca15@gmail.com', // receptor o receptores
+    subject: 'Somos Una empresa de desarrollo etc', // Asunto del correo
     text: 'Hello world ?', // correo en texto plano
+    html: '<h1>Prueba de correo super poderosa con html</h1>' // html body 
    
     //si se borra el atributo text se enviara tolo el html y viceversa
     // si se colocan los 2 html es prioridad en el mensaje
@@ -27,6 +29,7 @@ exports.enviar_email= function(req,res){
 transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
         return console.log(error);
+        res.status(500).send('error al enviar')
         }
 
         res.status(200).send('enviado con exito')
