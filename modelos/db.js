71fb6ab37c76ae3d76a197332d5f8e8dbcd60455
Cis <kalinca15@gmail.com>
2017-04-03 09:34:04 -0400

    Busqueda de elementos

--- modelos/db.js
+++ modelos/db.js
@@ -7,5 +7,13 @@ var pool = mysql.createPool({
     password: '',
     database: 'disenadorlogodb'
 });
-
+ /*
+var pool = mysql.createPool({
+    connectionLimit : 10,
+    host: 'localhost',
+    user: 'disena',
+    password: 'Esbz89%9',
+    database: 'disenadorlogodb'
+});
+*/
 module.exports = pool;
