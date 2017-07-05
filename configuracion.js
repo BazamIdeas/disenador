10273c2ed86c08e409ba9f7207fc0a964d3546b1
Cis <kalinca15@gmail.com>
2017-07-05 15:26:34 -0400

    variables de entorno

--- configuracion.js
+++ configuracion.js
@@ -1,32 +1,35 @@
 
-var produccion = {
-    "secret":"unaclavemuysegura",
-	"seguridad" : false,
-	"puerto" : "4001",
-	"url": "front.liderlogo.info",
-	"dashboard": "/creador-de-logos#!/area-del-cliente",
-    "paypal" : {
-    "host" : "api.sandbox.paypal.com",
-    "port" : "",            
-    "client_id" : "AVpLm3Mj781_AAa4M5gArCwllQ2LIv5WT6qccHJOjdbOMFOz_6fQmItQQbCWvXeeG3TS1qBA6a8_8NoV",  // your paypal application client id
-    "client_secret" : "EO-vJ68bnGFvig7xhITyMgtSYH24fb6tJqYL7VPFEdJ38B6cz0R6m-Bo3GMhYyVtCBkh6KLzW4k1KDlD" // your paypal application secret id
-  }
-};
+var node_env = process.env.NODE_ENV || 'desarrollo';
 
-var desarrollo = {
-    "secret":"unaclavemuysegura",
-	"seguridad" :false,
-	"puerto" : "8080",
-	"url": "http://localhost:8080",
-	"dashboard": "/creador-de-logos#!/area-del-cliente",
-	"paypal" : {
-    "host" : "api.sandbox.paypal.com",
-    "port" : "",            
-    "client_id" : "AVpLm3Mj781_AAa4M5gArCwllQ2LIv5WT6qccHJOjdbOMFOz_6fQmItQQbCWvXeeG3TS1qBA6a8_8NoV",  // your paypal application client id
-    "client_secret" : "EO-vJ68bnGFvig7xhITyMgtSYH24fb6tJqYL7VPFEdJ38B6cz0R6m-Bo3GMhYyVtCBkh6KLzW4k1KDlD" // your paypal application secret id
-  }
-};
+var configuracion = {
+    "produccion" : {
+        "secret":"unaclavemuysegura",
+    	"seguridad" : true,
+    	"puerto" : "4001",
+    	"url": "front.liderlogo.info",
+    	"dashboard": "/creador-de-logos#!/area-del-cliente",
+        "paypal" : {
+        "host" : "api.sandbox.paypal.com",
+        "port" : "",            
+        "client_id" : "AVpLm3Mj781_AAa4M5gArCwllQ2LIv5WT6qccHJOjdbOMFOz_6fQmItQQbCWvXeeG3TS1qBA6a8_8NoV",  // your paypal application client id
+        "client_secret" : "EO-vJ68bnGFvig7xhITyMgtSYH24fb6tJqYL7VPFEdJ38B6cz0R6m-Bo3GMhYyVtCBkh6KLzW4k1KDlD" // your paypal application secret id
+      }
+    },
 
-var configuracion = desarrollo;
+    "desarrollo" : {
+        "secret":"unaclavemuysegura",
+    	"seguridad" :true,
+    	"puerto" : "8080",
+    	"url": "http://localhost:8080",
+    	"dashboard": "/creador-de-logos#!/area-del-cliente",
+    	"paypal" : {
+        "host" : "api.sandbox.paypal.com",
+        "port" : "",            
+        "client_id" : "AVpLm3Mj781_AAa4M5gArCwllQ2LIv5WT6qccHJOjdbOMFOz_6fQmItQQbCWvXeeG3TS1qBA6a8_8NoV",  // your paypal application client id
+        "client_secret" : "EO-vJ68bnGFvig7xhITyMgtSYH24fb6tJqYL7VPFEdJ38B6cz0R6m-Bo3GMhYyVtCBkh6KLzW4k1KDlD" // your paypal application secret id
+      }
+    }
+}
 
-module.exports = configuracion;
+
+module.exports = configuracion[node_env];
