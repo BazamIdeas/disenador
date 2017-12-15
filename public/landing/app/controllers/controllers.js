99820836325b8c8959c0bcf4d8c50a1c81d8ea75
Cis <kalinca15@gmail.com>
2017-12-15 17:42:40 -0300

    / en vista xx

--- public/landing/app/controllers/controllers.js
+++ public/landing/app/controllers/controllers.js
@@ -13,7 +13,7 @@ angular.module("landing")
 
     bz.enviarComenzar = function (nombreLogo) {
 
-        bz.url = 'http://' + location.host + '/creador-de-logos#!/comenzar?n=' + nombreLogo;
+        bz.url = 'http://' + location.host + '/creador-de-logos#!/comenzar/?n=' + nombreLogo;
 
         $window.location.href = bz.url;
         // $window.open(bz.url, "_blank");
