0224a877a618e65f4e2be505d9fa923ac544709d
Xavier Arias <xarias13@gmail.com>
2018-01-05 13:43:49 -0400

    Redireccion movil a desktop y viceversa

--- public/creador-de-logos/app/directives.js
+++ public/creador-de-logos/app/directives.js
@@ -981,3 +981,36 @@ angular.module("disenador-de-logos")
         }
 
     })
+
+    /////////////////////////////////////////////
+    //////REDIRECCIONAR EN CAMBIO DE TAMAÑO//////
+    /////////////////////////////////////////////
+
+    .directive('bazamRedireccionar', function ($window, $location) {
+
+        return {
+            restrict: 'AE',
+            link: function (scope, element, attributes) {
+
+                if(true){
+                    
+                    if($window.innerWidth < 1024){
+                       
+                      $window.location = "/m" + $location.url();
+                    }
+                    
+                }
+                
+                angular.element(window).resize(function(){
+                                     
+                    if($window.innerWidth < 1024){
+                       
+                      $window.location = "/m" + $location.url();
+                    }
+                    
+                })
+
+            }
+        }
+
+    })
