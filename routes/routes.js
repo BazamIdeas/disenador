210e5565c08f5be587d4a931292262ff62e71693
Daniel Torres <danieljtorres94@gmail.com>
2018-01-25 15:45:11 -0300

    Corrrecion clientes freelancer

--- routes/routes.js
+++ routes/routes.js
@@ -10,7 +10,7 @@ var configuracion       = require('../configuracion.js');
 //MODULO CLIENTES
 //no espera parametros
 router.get('/clientes', middleware.validarAdministrador, controllers.clientes.listaClientes);
-router.get('/clientes/freelancer', middleware.validarAdministrador, controllers.clientes.listaClientesFreelancer);
+router.get('/clientes/freelancer'/*, middleware.validarAdministrador*/, controllers.clientes.listaClientesFreelancer);
 router.get('/cliente/saldo-personal', middleware.validarCliente, controllers.pagos.SaldoPorCliente);
 router.get('/cliente/saldo', middleware.validarAdministrador, controllers.pagos.SaldoPorCliente);
 //parametro por get que debe ser el id del cliente.
