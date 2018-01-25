d43dcfdc0a88f1331c06977ad1cfcecc86b7c53e
Daniel Torres <danieljtorres94@gmail.com>
2018-01-25 09:56:58 -0300

    Todos los pagos

--- modelos/pagosModelo.js
+++ modelos/pagosModelo.js
@@ -3,6 +3,25 @@ var DB = require('./db.js');
 var pago = {};
 
 pago.ObtenerPorCliente = function(idCliente, callback) {
+	
+	var q = "SELECT * FROM pagos WHERE pagos.clientes_idCliente = ?"
+	var par = [idCliente] //parametros
+
+	DB.getConnection(function(err, connection)
+	{
+		connection.query( q , par , function(err, rows){
+	  	
+		  	if(err)	throw err;
+		  	
+		  	else callback(null, rows);
+
+		  	connection.release();
+	  	
+	 	});
+	});
+}
+
+pago.ObtenerPorCliente = function(idCliente, callback) {
 
     var q = `SELECT pagos.fecha, pagos.monto, facturacion.medio, facturacion.correo FROM pagos INNER JOIN facturacion ON pagos.facturacion_idFacturacion = facturacion.idFacturacion WHERE facturacion.clientes_idCliente = ?`;
 	var par = [idCliente];
