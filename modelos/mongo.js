8d756737d1672db198affefc67f90376da5c627f
Daniel Torres <danieljtorres94@gmail.com>
2018-04-13 12:00:36 -0300

    conexion mongo por ssh tunel

--- modelos/mongo.js
+++ modelos/mongo.js
@@ -1,21 +1,57 @@
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectID;
+const tunnel = require('tunnel-ssh');
 
-const cfg = require("../configuracion/configuracion.js");
+const config = require("../configuracion/configuracion.js");
+const node_env = process.env.NODE_ENV || 'desarrollo';
 
-// Connection URL
-const datos = cfg.mongo;
+const datos = config.mongo;
 
-// Use connect method to connect to the server
-const mongo = {
-    connection: callback => { 
+const connection = {
+    test: callback => { 
+        var server = tunnel(datos.tunnel, function(error, server){
+            MongoClient.connect(datos.url, (err, client) => {
+                if (err) throw err;
+                const db = client.db(datos.database);
+                callback(db)
+                client.close();
+            })
+        })
+
+        server.on('error', function(err){
+            console.error('Something bad happened:', err);
+        });
+
+    },
+
+    desarrollo: callback => { 
+        var server = tunnel(datos.tunnel, function(error, server){
+            MongoClient.connect(datos.url, (err, client) => {
+                if (err) throw err;
+                const db = client.db(datos.database);
+                callback(db)
+                client.close();
+            })
+        })
+
+        server.on('error', function(err){
+            console.error('Something bad happened:', err);
+        });
+
+    },
+
+    produccion: callback => { 
         MongoClient.connect(datos.url, (err, client) => {
             if (err) throw err;
             const db = client.db(datos.database);
             callback(db)
             client.close();
         })
-    },
+    }
+}
+
+const mongo = {
+    connection: connection[node_env],
     objectId: id => {
         return new ObjectId(id);
     }
