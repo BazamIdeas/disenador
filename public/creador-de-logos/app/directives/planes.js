4765e7eeef019a9fd6f494d586fb16a89f2b75f2
Luis Torres <luisdaniel.programador@gmail.com>
2018-04-25 10:14:05 -0300

    Bloqueo de pantalla

--- public/creador-de-logos/app/directives/planes.js
+++ public/creador-de-logos/app/directives/planes.js
@@ -95,7 +95,6 @@ angular.module("disenador-de-logos")
                 };
 
                 bz.verificarLogin = function (plan) {
-                    angular.element(document.querySelector(".overlay.full")).fadeIn(1000);
                     bz.planElegido = plan;
                     // Verificar si el usuario que esta logueado
                     if (!clientesService.autorizado()) {
@@ -109,7 +108,7 @@ angular.module("disenador-de-logos")
 
 
                 bz.avanzarCheckout = function () {
-                    angular.element(document.querySelector(".overlay.full")).fadeIn(1000);
+                   
                     var plan = bz.planElegido;
 
                     bz.logo = $scope.datos.logo; //SVG del logo
@@ -121,6 +120,7 @@ angular.module("disenador-de-logos")
                     bz.colores = $scope.datos.colores;
 
                     if (plan === true) {
+                        angular.element(document.querySelector(".bloquear-pantalla")).fadeIn(1000);
                         bz.peticion = true;
                         var nombre = "gratis";
                         var ancho = 80;
@@ -204,7 +204,7 @@ angular.module("disenador-de-logos")
                                         if ($window.navigator && $window.navigator.msSaveOrOpenBlob) {
                                             $window.navigator.msSaveOrOpenBlob(blob, fileName);
                                         } else {
-                                            angular.element(document.querySelector(".overlay.full")).fadeOut(1000);
+                                            angular.element(document.querySelector(".bloquear-pantalla")).fadeOut(1000);
                                             var a = $document[0].createElement("a");
                                             $document[0].body.appendChild(a);
                                             a.style = "display:none";
@@ -219,7 +219,7 @@ angular.module("disenador-de-logos")
 
                                     }).catch(function () {
                                         //console.log(res)
-                                        angular.element(document.querySelector(".overlay.full")).fadeOut(1000);
+                                        angular.element(document.querySelector(".bloquear-pantalla")).fadeOut(1000);
                                     }).finally(function () {
                                         
 
@@ -233,7 +233,7 @@ angular.module("disenador-de-logos")
 
                         }).catch(function (res) {
                             
-                            angular.element(document.querySelector(".overlay.full")).fadeOut(1000);
+                            angular.element(document.querySelector(".bloquear-pantalla")).fadeOut(1000);
 
                             if (res === "exceso") {
                                 $mdToast.show($mdToast.base({
