832533f23384c3d65b3361e94ac0d87042a6f8c3
Daniel Torres <danieljtorres94@gmail.com>
2018-07-13 17:06:23 -0300

    etiqueta lang

--- controllers/etiquetas.js
+++ controllers/etiquetas.js
@@ -18,7 +18,7 @@ exports.ObtenerTodos = (req, res) =>
 
 
 exports.ObtenerTodosDeIdioma = (req, res) => {
-	Etiqueta.ObtenerTodoDeIdioma(req.cookies.lang || 'es', (err, data) => {
+	Etiqueta.ObtenerTodoDeIdioma(req.lang , (err, data) => {
 		if (data.length > 0) {
 			res.status(200).json(data);
 		} else {
